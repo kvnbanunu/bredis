db7ba25b14942c5be9340ab76ef41aa99411ceca
kvnbanunu <kvnbanunu@gmail.com>
2024-06-10 00:29:52 -0700

    Created a simple protocol to split request
    The requests are split by declaring how long the request is at the beginning of the request.

--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <assert.h>
 #include <errno.h>
 #include <netinet/ip.h>
 #include <stdint.h>
@@ -9,9 +10,7 @@
 #include <unistd.h>
 
 // Prints error message
-static void msg(const char *msg) {
-  fprintf(stderr, "%s\n", msg);
-}
+static void msg(const char *msg) { fprintf(stderr, "%s\n", msg); }
 
 // Prints an error message and the last error code
 // Intended to be used when the program should terminate due to an error
@@ -20,18 +19,76 @@ static void die(const char *msg) {
   fprintf(stderr, "[%d] %s\n", err, msg);
 }
 
-// Read & Write
-static void do_something(int connfd) {
-  char rbuf[64] = {};
-  ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
-  if (n < 0) {
-    msg("read() error");
-    return;
-  }
-  printf("client says: %s\n", rbuf);
+const size_t k_max_msg = 4096;
 
-  char wbuf[] = "world";
-  write(connfd, wbuf, strlen(wbuf));
+// reads from the kernal until it gets exactly n bytes
+static int32_t read_full(int fd, char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = read(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error, or unexpected EOF
+    }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+  return 0;
+}
+
+static int32_t write_all(int fd, const char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = write(fd, buf, n);
+    if (rv <= 0) {
+      return -1; // error
+    }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+
+  return 0;
+}
+
+static int32_t one_request(int connfd) {
+
+  // 4 bytes header
+  char rbuf[4 + k_max_msg + 1];
+  errno = 0;
+  int32_t err = read_full(connfd, rbuf, 4);
+  if (err) {
+    if (errno == 0) {
+      msg("EOF");
+    } else {
+      msg("read() error");
+    }
+    return err;
+  }
+
+  uint32_t len = 0;
+  memcpy(&len, rbuf, 4); // assume little endian
+  if (len > k_max_msg) {
+    msg("too long");
+    return -1;
+  }
+
+  // request body
+  err = read_full(connfd, &rbuf[4], len);
+  if (err) {
+    msg("read() error");
+    return err;
+  }
+
+  // do something
+  rbuf[4 + len] = '\0';
+  printf("client says: %s\n", &rbuf[4]);
+
+  // reply using the same protocol
+  const char reply[] = "world";
+  char wbuf[4 + sizeof(reply)];
+  len = (uint32_t)strlen(reply);
+  memcpy(wbuf, &len, 4);
+  memcpy(&wbuf[4], reply, len);
+  return write_all(connfd, wbuf, 4 + len);
 }
 
 int main() {
@@ -58,7 +115,7 @@ int main() {
     die("bind()");
   }
 
-  // Listen
+  // Listen for connections
   rv = listen(fd, SOMAXCONN); // SOMAXCONN is the size of the queue (128)
   if (rv) {
     die("listen()");
@@ -73,7 +130,14 @@ int main() {
       continue; // error
     }
 
-    do_something(connfd);
+    // only serves one client connection at once
+    while (true) {
+      int32_t err = one_request(connfd);
+      if (err) {
+        break;
+      }
+    }
+
     close(connfd);
   }
 
